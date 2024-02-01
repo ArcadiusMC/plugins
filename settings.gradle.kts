0b9469cf276071cac50bdf3d57fba9e17c9ae775
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2024-02-01 13:34:26 +0200

    Update gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,14 @@ rootProject.name = "arcadius-plugins"
 include("class-loader-tools")
 include("commons")
 include("core")
+include("menus")
 
 include("scripting-impl")
 include("scripting")
 
 include("vanilla-hook")
+
+include("extended-items")
+include("user-titles")
+include("server-list")
+include("dialogues")
