6d09b5612d030a54a982d7939dfad461942557b4
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2024-02-01 13:33:37 +0200

    Create `extended-items` plugin

---
+++ extended-items/src/main/java/net/arcadiusmc/customitems/ItemPlugin.java
@@ -0,0 +1,16 @@
+package net.arcadiusmc.customitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ItemPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
