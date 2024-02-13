eab5a6b7c70ea4280c7e4336f9b06c43e636396d
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2024-02-13 23:25:57 +0200

    remove `combine`

--- core/src/main/java/net/arcadiusmc/core/user/NameElements.java
+++ core/src/main/java/net/arcadiusmc/core/user/NameElements.java
@@ -1,10 +1,8 @@
 package net.arcadiusmc.core.user;
 
-import net.arcadiusmc.text.Text;
 import net.arcadiusmc.user.Properties;
-import net.arcadiusmc.user.name.UserNameFactory;
 import net.arcadiusmc.user.name.NameElement;
-import net.kyori.adventure.text.Component;
+import net.arcadiusmc.user.name.UserNameFactory;
 
 public final class NameElements {
   private NameElements() {}
@@ -18,17 +16,5 @@ public final class NameElements {
   public static void registerAll(UserNameFactory factory) {
     factory.addPrefix("prefix_property", 0, PREFIX_PROPERTY);
     factory.addSuffix("suffix_property", 0, SUFFIX_PROPERTY);
-  }
-
-  public static NameElement combine(NameElement n1, NameElement n2) {
-    return (user, context) -> {
-      Component first = n1.createDisplay(user, context);
-
-      if (Text.isEmpty(first)) {
-        return n2.createDisplay(user, context);
-      }
-
-      return first;
-    };
   }
 }
