7a6c49cca83435b211134118c48fd3c567e19a53
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2024-02-18 00:53:58 +0200

    Update core's user-preferences permissions

--- core/src/main/java/net/arcadiusmc/core/PrefsBook.java
+++ core/src/main/java/net/arcadiusmc/core/PrefsBook.java
@@ -1,9 +1,11 @@
 package net.arcadiusmc.core;
 
 import java.util.List;
+import net.arcadiusmc.Permissions;
 import net.arcadiusmc.command.settings.BookSetting;
 import net.arcadiusmc.command.settings.Setting;
 import net.arcadiusmc.command.settings.SettingsBook;
+import net.arcadiusmc.core.commands.tpa.TpPermissions;
 import net.arcadiusmc.user.Properties;
 import net.arcadiusmc.user.User;
 import net.arcadiusmc.user.UserProperty;
@@ -21,22 +23,27 @@ public class PrefsBook {
 
   static void init(SettingsBook<User> settings) {
     Setting flying = Setting.create(Properties.FLYING)
-        .setMessageKey("settings.fly");
+        .setMessageKey("settings.fly")
+        .setPermission(CorePermissions.FLY);
 
     Setting god = Setting.create(Properties.GODMODE)
-        .setMessageKey("settings.god");
+        .setMessageKey("settings.god")
+        .setPermission(CorePermissions.GOD);
 
     Setting tpa = Setting.create(Properties.TPA)
-        .setMessageKey("settings.tpa");
+        .setMessageKey("settings.tpa")
+        .setPermission(TpPermissions.TPA);
 
     Setting profilePrivate = Setting.create(Properties.PROFILE_PRIVATE)
-        .setMessageKey("settings.profilePrivate");
+        .setMessageKey("settings.profilePrivate")
+        .setPermission(Permissions.PROFILE);
 
     Setting ignoreac = Setting.createInverted(IGNORE_AUTO_BROADCASTS)
         .setMessageKey("settings.showAnnouncements");
 
     Setting paying = Setting.create(PAY)
-        .setMessageKey("settings.paying");
+        .setMessageKey("settings.paying")
+        .setPermission(CorePermissions.PAY);
 
     Setting durabilityAlerts = Setting.create(DURABILITY_WARNINGS)
         .setMessageKey("settings.durabilityWarn");
