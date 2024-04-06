7fb6c01d16239e76947bf839b50f760da452961c
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2024-04-06 03:00:08 +0300

    Change teleport usables action

--- usables/src/main/java/net/arcadiusmc/usables/actions/TeleportAction.java
+++ usables/src/main/java/net/arcadiusmc/usables/actions/TeleportAction.java
@@ -1,103 +1,53 @@
 package net.arcadiusmc.usables.actions;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.Dynamic;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.doubles.DoubleList;
+import it.unimi.dsi.fastutil.floats.FloatList;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import lombok.Getter;
+import net.arcadiusmc.Loggers;
 import net.arcadiusmc.text.Text;
 import net.arcadiusmc.usables.Action;
-import net.arcadiusmc.usables.BuiltType;
 import net.arcadiusmc.usables.Interaction;
 import net.arcadiusmc.usables.ObjectType;
 import net.arcadiusmc.usables.UsableComponent;
-import net.arcadiusmc.usables.objects.Warp;
 import net.arcadiusmc.user.User;
 import net.arcadiusmc.user.UserTeleport.Type;
 import net.arcadiusmc.user.Users;
 import net.arcadiusmc.utils.Locations;
-import net.arcadiusmc.utils.io.TagUtil;
+import net.arcadiusmc.utils.io.ExtraCodecs;
+import net.forthecrown.grenadier.CommandSource;
 import net.forthecrown.grenadier.types.ArgumentTypes;
-import net.forthecrown.grenadier.types.ParsedPosition;
 import net.forthecrown.grenadier.types.options.ArgumentOption;
 import net.forthecrown.grenadier.types.options.Options;
 import net.forthecrown.grenadier.types.options.OptionsArgument;
 import net.forthecrown.grenadier.types.options.ParsedOptions;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.util.TriState;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 
+@Getter
 public class TeleportAction implements Action {
 
-  private static final ArgumentOption<Float> YAW
-      = Options.argument(FloatArgumentType.floatArg(-180, 180))
-      .setLabel("yaw")
-      .build();
+  private static final Logger LOGGER = Loggers.getLogger();
 
-  private static final ArgumentOption<Float> PITCH
-      = Options.argument(FloatArgumentType.floatArg(-90, 90))
-      .setLabel("pitch")
-      .build();
-
-  private static final ArgumentOption<World> WORLD
-      = Options.argument(ArgumentTypes.world())
-      .setLabel("world")
-      .build();
-
-  private static final ArgumentOption<ParsedPosition> POS_ARG
-      = Options.argument(ArgumentTypes.position())
-      .setLabel("pos")
-      .setDefaultValue(ParsedPosition.IDENTITY)
-      .build();
-
-  private static final OptionsArgument ARGS = OptionsArgument.builder()
-      .addOptional(YAW)
-      .addOptional(PITCH)
-      .addOptional(WORLD)
-      .addOptional(POS_ARG)
-      .build();
-
-  public static final ObjectType<TeleportAction> TYPE = BuiltType.<TeleportAction>builder()
-      .requiresInput(TriState.FALSE)
-
-      .tagLoader(binaryTag -> new TeleportAction(TagUtil.readLocation(binaryTag)))
-      .tagSaver(action -> TagUtil.writeLocation(action.location))
-
-      .parser((reader, source) -> {
-        Location loc;
-
-        if (reader.canRead()) {
-          ParsedOptions options = ARGS.parse(reader).checkAccess(source);
-
-          if (options.has(POS_ARG)) {
-            ParsedPosition pos = options.getValue(POS_ARG);
-            loc = pos.apply(source);
-          } else {
-            loc = source.getLocation();
-          }
-
-          if (options.has(YAW)) {
-            loc.setYaw(options.getValue(YAW));
-          }
-
-          if (options.has(PITCH)) {
-            loc.setPitch(options.getValue(PITCH));
-          }
-
-          if (options.has(WORLD)) {
-            loc.setWorld(options.getValue(WORLD));
-          } else {
-            loc.setWorld(null);
-          }
-        } else {
-          loc = source.getLocation();
-        }
-
-        return new TeleportAction(loc);
-      })
-      .suggester(ARGS::listSuggestions)
-      .applicableTo(object -> object.as(Warp.class).isEmpty())
-      .build();
+  static final TeleportActionType TYPE = new TeleportActionType();
 
   private final Location location;
 
@@ -124,6 +74,7 @@ public class TeleportAction implements Action {
     user.createTeleport(() -> destination, Type.TELEPORT)
         .setSilent(interaction.getBoolean("silent").orElse(false))
         .setDelay(null)
+        .setSetReturn(false)
         .start();
   }
 
@@ -135,5 +86,131 @@ public class TeleportAction implements Action {
   @Override
   public @Nullable Component displayInfo() {
     return Text.clickableLocation(location, true);
+  }
+}
+
+class TeleportActionType implements ObjectType<TeleportAction> {
+
+  private static final ArgumentOption<Float> YAW
+      = Options.argument(FloatArgumentType.floatArg(-180, 180))
+      .setLabel("yaw")
+      .build();
+
+  private static final ArgumentOption<Float> PITCH
+      = Options.argument(FloatArgumentType.floatArg(-90, 90))
+      .setLabel("pitch")
+      .build();
+
+  private static final ArgumentOption<World> WORLD
+      = Options.argument(ArgumentTypes.world())
+      .setLabel("world")
+      .build();
+
+  private static final ArgumentOption<Double> X
+      = Options.argument(DoubleArgumentType.doubleArg(), "x");
+
+  private static final ArgumentOption<Double> Y
+      = Options.argument(DoubleArgumentType.doubleArg(), "y");
+
+  private static final ArgumentOption<Double> Z
+      = Options.argument(DoubleArgumentType.doubleArg(), "z");
+
+
+  private static final OptionsArgument ARGS = OptionsArgument.builder()
+      .addOptional(YAW)
+      .addOptional(PITCH)
+      .addOptional(WORLD)
+      .addRequired(X)
+      .addRequired(Y)
+      .addRequired(Z)
+      .build();
+
+  static final Codec<TeleportAction> CODEC = ExtraCodecs.LOCATION_CODEC
+      .xmap(TeleportAction::new, TeleportAction::getLocation);
+
+  static final Codec<Location> OLD_CODEC = RecordCodecBuilder.create(instance -> {
+    return instance
+        .group(
+            ExtraCodecs.WORLD_CODEC.optionalFieldOf("world")
+                    .forGetter(l -> Optional.ofNullable(l.getWorld())),
+
+            Codec.DOUBLE.listOf().fieldOf("pos")
+                .forGetter(l -> DoubleList.of(l.x(), l.y(), l.z())),
+
+            Codec.FLOAT.listOf().optionalFieldOf("rot", FloatList.of(0, 0))
+                .forGetter(l -> FloatList.of(l.getYaw(), l.getPitch()))
+        )
+        .apply(instance, (world, pos, rot) -> {
+          return new Location(
+              world.orElse(null),
+              pos.get(0),
+              pos.get(1),
+              pos.get(2),
+              rot.get(0),
+              rot.get(1)
+          );
+        });
+  });
+
+  @Override
+  public TeleportAction parse(StringReader reader, CommandSource source)
+      throws CommandSyntaxException
+  {
+    double x;
+    double y;
+    double z;
+    World world;
+    Float yaw;
+    Float pitch;
+
+    if (reader.canRead()) {
+      ParsedOptions options = ARGS.parse(reader);
+
+      x = options.getValue(X);
+      y = options.getValue(Y);
+      z = options.getValue(Z);
+
+      world = options.getValueOptional(WORLD).orElseGet(source::getWorld);
+
+      yaw = options.getValue(YAW);
+      pitch = options.getValue(PITCH);
+    } else {
+      Location loc = source.getLocation();
+
+      x = loc.x();
+      y = loc.y();
+      z = loc.z();
+      world = loc.getWorld();
+      yaw = loc.getYaw();
+      pitch = loc.getPitch();
+    }
+
+    return new TeleportAction(new Location(world, x, y, z, yaw, pitch));
+  }
+
+  @Override
+  public CompletableFuture<Suggestions> getSuggestions(
+      CommandContext<CommandSource> context,
+      SuggestionsBuilder builder
+  ) {
+    return ARGS.listSuggestions(context, builder);
+  }
+
+  private <S> Dynamic<S> fix(Dynamic<S> dynamic) {
+    return OLD_CODEC.parse(dynamic)
+        .flatMap(location -> ExtraCodecs.LOCATION_CODEC.encodeStart(dynamic.getOps(), location))
+        .map(s -> new Dynamic<>(dynamic.getOps(), s))
+        .result()
+        .orElse(dynamic);
+  }
+
+  @Override
+  public <S> DataResult<TeleportAction> load(Dynamic<S> dynamic) {
+    return CODEC.parse(fix(dynamic));
+  }
+
+  @Override
+  public <S> DataResult<S> save(@NotNull TeleportAction value, @NotNull DynamicOps<S> ops) {
+    return CODEC.encodeStart(ops, value);
   }
 }
