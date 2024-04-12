99c6de8f7438396403e8e5ffdfcdd11fad38c0a4
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2024-04-12 12:47:16 +0300

    Add hologram content placeholder

--- holograms/src/main/java/net/arcadiusmc/holograms/placeholders/HologramPlaceholders.java
+++ holograms/src/main/java/net/arcadiusmc/holograms/placeholders/HologramPlaceholders.java
@@ -1,7 +1,6 @@
 package net.arcadiusmc.holograms.placeholders;
 
 import com.google.common.base.Strings;
-import net.arcadiusmc.holograms.BoardImpl;
 import net.arcadiusmc.holograms.Holograms;
 import net.arcadiusmc.text.placeholder.PlaceholderContext;
 import net.arcadiusmc.text.placeholder.PlaceholderList;
@@ -14,22 +13,23 @@ import org.jetbrains.annotations.Nullable;
 public class HologramPlaceholders {
 
   static final String LB = "lb";
+  static final String HOLOGRAM = "hologram";
 
   public static void registerAll() {
     PlaceholderService service = Placeholders.getService();
     PlaceholderList defaults = service.getDefaults();
     defaults.add(LB, LeaderboardPlaceholder.PLACEHOLDER);
+    defaults.add(HOLOGRAM, HologramPlaceholder.PLACEHOLDER);
   }
 
   public static void unregister() {
     PlaceholderService service = Placeholders.getService();
     PlaceholderList defaults = service.getDefaults();
     defaults.remove(LB);
+    defaults.remove(HOLOGRAM);
   }
 
-  private enum LeaderboardPlaceholder
-      implements TextPlaceholder
-  {
+  private enum HologramPlaceholder implements TextPlaceholder {
     PLACEHOLDER;
 
     @Override
@@ -38,10 +38,24 @@ public class HologramPlaceholders {
         return null;
       }
 
-      return Holograms.getService().getLeaderboard(match)
-          .map(leaderboard -> {
-            return (BoardImpl) leaderboard;
-          })
+      return Holograms.getService()
+          .getHologram(match)
+          .map(display -> display.renderText(render.viewer()))
+          .orElse(null);
+    }
+  }
+
+  private enum LeaderboardPlaceholder implements TextPlaceholder {
+    PLACEHOLDER;
+
+    @Override
+    public @Nullable Component render(String match, PlaceholderContext render) {
+      if (Strings.isNullOrEmpty(match)) {
+        return null;
+      }
+
+      return Holograms.getService()
+          .getLeaderboard(match)
           .map(board -> board.renderText(render.viewer()))
           .orElse(null);
     }
