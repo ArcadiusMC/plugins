ffda54d9b5c6f2817b86618a8701768fd510e1b8
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2024-04-12 22:37:03 +0300

    Add MarketPurchaseAttemptEvent

---
+++ player-markets/src/main/java/net/arcadiusmc/markets/event/MarketPurchaseAttemptEvent.java
@@ -0,0 +1,36 @@
+package net.arcadiusmc.markets.event;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.arcadiusmc.markets.Market;
+import net.arcadiusmc.user.User;
+import net.arcadiusmc.user.event.UserEvent;
+import net.kyori.adventure.text.Component;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class MarketPurchaseAttemptEvent extends UserEvent implements Cancellable {
+
+  @Getter
+  private static HandlerList handlerList = new HandlerList();
+
+  @Getter
+  private final Market market;
+
+  @Getter @Setter
+  private Component denyReason;
+
+  @Getter @Setter
+  private boolean cancelled;
+
+  public MarketPurchaseAttemptEvent(User user, Market market) {
+    super(user);
+    this.market = market;
+  }
+
+  @Override
+  public @NotNull HandlerList getHandlers() {
+    return handlerList;
+  }
+}
--- player-markets/src/main/java/net/arcadiusmc/markets/gui/ClaimingBook.java
+++ player-markets/src/main/java/net/arcadiusmc/markets/gui/ClaimingBook.java
@@ -8,7 +8,9 @@ import net.arcadiusmc.markets.Markets;
 import net.arcadiusmc.markets.MarketsConfig;
 import net.arcadiusmc.markets.MarketsManager;
 import net.arcadiusmc.markets.MarketsPlugin;
+import net.arcadiusmc.markets.event.MarketPurchaseAttemptEvent;
 import net.arcadiusmc.text.Messages;
+import net.arcadiusmc.text.Text;
 import net.arcadiusmc.text.UnitFormat;
 import net.arcadiusmc.text.UserClickCallback;
 import net.arcadiusmc.user.TimeField;
@@ -57,16 +59,27 @@ public final class ClaimingBook {
   }
 
   private static Component purchaseButton(Market market, User user, int price) {
+    MarketPurchaseAttemptEvent event = callPurchaseEvent(user, market, price);
+
+    boolean canPurchase = event.isCancelled();
+    Component denyReason = event.getDenyReason();
+
     ClickEvent clickEvent = ClickEvent.callback(
         (UserClickCallback) user1 -> attemptPurchase(market, user1, price),
         builder -> builder.uses(1)
     );
 
-    String key = user.hasBalance(price) ? "available" : "unavailable";
+    String key = canPurchase ? "available" : "unavailable";
 
-    return Messages.render("markets.gui.buttons.rent", key)
+    Component result = Messages.render("markets.gui.buttons.rent", key)
         .create(user)
         .clickEvent(clickEvent);
+
+    if (!canPurchase && !Text.isEmpty(denyReason)) {
+      result = result.hoverEvent(denyReason);
+    }
+
+    return result;
   }
 
   private static void attemptPurchase(Market market, User user, int price)
@@ -74,6 +87,17 @@ public final class ClaimingBook {
   {
     if (!user.hasBalance(price)) {
       throw Exceptions.cannotAfford(user, price);
+    }
+
+    MarketPurchaseAttemptEvent event = callPurchaseEvent(user, market, price);
+    if (event.isCancelled()) {
+      Component reason = event.getDenyReason();
+
+      if (Text.isEmpty(reason)) {
+        throw Messages.render("markets.errors.purchaseForbidden").exception(user);
+      }
+
+      throw Exceptions.create(event.getDenyReason());
     }
 
     MarketsManager manager = market.getManager();
@@ -99,5 +123,18 @@ public final class ClaimingBook {
             .addValue("rentInterval", config.rentInterval())
             .create(user)
     );
+  }
+
+  private static MarketPurchaseAttemptEvent callPurchaseEvent(User user, Market market, int price) {
+    MarketPurchaseAttemptEvent event = new MarketPurchaseAttemptEvent(user, market);
+
+    if (!user.hasBalance(price)) {
+      event.setCancelled(true);
+      event.setDenyReason(Exceptions.cannotAffordText(user, price));
+    }
+
+    event.callEvent();
+
+    return event;
   }
 }
