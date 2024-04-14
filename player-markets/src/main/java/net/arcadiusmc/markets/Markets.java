57989930f4911d5fd1d7f0b536275de763ea288d
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2024-04-14 15:21:52 +0300

    Change actionCooldown validation to be more reusable

--- player-markets/src/main/java/net/arcadiusmc/markets/Markets.java
+++ player-markets/src/main/java/net/arcadiusmc/markets/Markets.java
@@ -7,11 +7,26 @@ import net.arcadiusmc.command.Exceptions;
 import net.arcadiusmc.text.Messages;
 import net.arcadiusmc.user.TimeField;
 import net.arcadiusmc.user.User;
+import net.kyori.adventure.text.Component;
 
 public final class Markets {
   private Markets() {}
 
   public static void validateActionCooldown(User user) throws CommandSyntaxException {
+    Component msg = actionCooldown(user);
+
+    if (msg == null) {
+      return;
+    }
+
+    throw Exceptions.create(msg);
+  }
+
+  public static boolean testActionCooldown(User user) {
+    return actionCooldown(user) == null;
+  }
+
+  public static Component actionCooldown(User user) {
     MarketsPlugin plugin = MarketsPlugin.plugin();
     MarketsConfig config = plugin.getPluginConfig();
 
@@ -23,21 +38,10 @@ public final class Markets {
 
     if (now.isBefore(nextAllowed)) {
       Duration remaining = Duration.between(now, nextAllowed);
-      throw Exceptions.create(Messages.cooldownMessage(user, remaining, interval));
+      return Messages.cooldownMessage(user, remaining, interval);
     }
-  }
 
-  public static boolean testActionCooldown(User user) {
-    MarketsPlugin plugin = MarketsPlugin.plugin();
-    MarketsConfig config = plugin.getPluginConfig();
-
-    Duration interval = config.actionCooldown();
-
-    Instant lastAction = Instant.ofEpochMilli(user.getTime(TimeField.MARKET_LAST_ACTION));
-    Instant now = Instant.now();
-    Instant nextAllowed = lastAction.plus(interval);
-
-    return now.isAfter(nextAllowed);
+    return null;
   }
 
   public static MarketsManager getManager() {
