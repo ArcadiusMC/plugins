faecad4c9005e5a5b13d5eb895dbfeffdc9bbacb
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2024-04-16 21:16:40 +0300

    Fix SellShop no items message mistype

--- sell-shop/src/main/java/net/arcadiusmc/sellshop/SellErrors.java
+++ sell-shop/src/main/java/net/arcadiusmc/sellshop/SellErrors.java
@@ -4,6 +4,6 @@ import net.arcadiusmc.text.Messages;
 import net.arcadiusmc.text.loader.MessageRef;
 
 public interface SellErrors {
-  MessageRef NO_ITEM_TO_SELL = Messages.reference("sellshop.noItems");
+  MessageRef NO_ITEM_TO_SELL = Messages.reference("sellshop.errors.noItems");
   MessageRef CANNOT_SELL_MORE = Messages.reference("sellshop.cannotSellMore");
 }
