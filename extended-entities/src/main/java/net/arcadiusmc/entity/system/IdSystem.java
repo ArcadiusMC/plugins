839b078c99cad9eb02e905328e71079d85826443
andero-magi <andero.magi.420+schoolgit@gmail.com>
2024-05-26 14:01:28 +0300

    Ensure IdSystem is better up to date with entity IDs

--- extended-entities/src/main/java/net/arcadiusmc/entity/system/IdSystem.java
+++ extended-entities/src/main/java/net/arcadiusmc/entity/system/IdSystem.java
@@ -4,18 +4,31 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntityListener;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.signals.Listener;
+import com.badlogic.ashley.signals.Signal;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
+import net.arcadiusmc.Loggers;
+import org.slf4j.Logger;
 
 public class IdSystem extends EntitySystem {
 
+  private static final Logger LOGGER = Loggers.getLogger();
+
   private final Map<UUID, Entity> byId = new Object2ObjectOpenHashMap<>();
+  private final Map<Entity, UUID> byEntity = new Object2ObjectOpenHashMap<>();
 
   private final IdRegisteringListener listener = new IdRegisteringListener();
+  private final ComponentListener componentListener = new ComponentListener();
 
   public Entity getEntity(UUID uuid) {
     return byId.get(uuid);
+  }
+
+  public UUID getId(Entity entity) {
+    return byEntity.get(entity);
   }
 
   @Override
@@ -28,6 +41,34 @@ public class IdSystem extends EntitySystem {
     engine.removeEntityListener(listener);
   }
 
+  class ComponentListener implements Listener<Entity> {
+
+    @Override
+    public void receive(Signal<Entity> signal, Entity object) {
+      EntityId id = object.getComponent(EntityId.class);
+
+      if (id == null) {
+        UUID existing = byEntity.remove(object);
+
+        if (existing != null) {
+          byId.remove(existing);
+        }
+
+        return;
+      }
+
+      UUID uuid = id.getId();
+      Entity found = byId.get(uuid);
+
+      if (found == null) {
+        byEntity.put(object, uuid);
+        byId.put(uuid, object);
+      } else if (!Objects.equals(found, object)) {
+        LOGGER.warn("Found entities with conflicting IDs, id={}", uuid);
+      }
+    }
+  }
+
   class IdRegisteringListener implements EntityListener {
 
     @Override
@@ -35,7 +76,17 @@ public class IdSystem extends EntitySystem {
       EntityId id = entity.getComponent(EntityId.class);
       if (id == null) {
         return;
+
       }
+
+      Entity existing = byId.get(id.id);
+
+      if (existing != null && !Objects.equals(existing, entity)) {
+        LOGGER.warn("Entity add with duplicate ID, id={}", id.id);
+      }
+
+      entity.componentAdded.add(componentListener);
+      entity.componentRemoved.add(componentListener);
 
       byId.put(id.id, entity);
     }
