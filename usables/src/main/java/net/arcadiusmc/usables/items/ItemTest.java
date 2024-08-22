69a5bd3ec98bc0e7660e765a3a5846d45efb7466
andero-magi <andero.magi.420@gmail.com>
2024-08-22 19:08:07 +0300

    Add missing failure message to `has_items` and `does_not_have_items` conditions

--- usables/src/main/java/net/arcadiusmc/usables/items/ItemTest.java
+++ usables/src/main/java/net/arcadiusmc/usables/items/ItemTest.java
@@ -1,9 +1,13 @@
 package net.arcadiusmc.usables.items;
 
+import net.arcadiusmc.text.TextWriter;
+import net.arcadiusmc.text.TextWriters;
 import net.arcadiusmc.usables.Condition;
 import net.arcadiusmc.usables.Interaction;
 import net.arcadiusmc.usables.ObjectType;
 import net.arcadiusmc.usables.UsableComponent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
@@ -36,5 +40,38 @@ public class ItemTest extends ItemComponent implements Condition {
   @Override
   public ObjectType<? extends UsableComponent> getType() {
     return required ? ItemTestType.CONTAINS : ItemTestType.NOT;
+  }
+
+  @Override
+  public Component failMessage(Interaction interaction) {
+    TextWriter writer = TextWriters.newWriter();
+
+    boolean written = consumeInteraction(interaction, (player, itemStacks) -> {
+      PlayerInventory inventory = player.getInventory();
+
+      if (required) {
+        writer.line("You need the following items:", NamedTextColor.GRAY);
+      } else {
+        writer.line("You must &lnot&r have the following items:", NamedTextColor.GRAY);
+      }
+
+      for (ItemStack itemStack : itemStacks) {
+        boolean contains = inventory.containsAtLeast(itemStack, itemStack.getAmount());
+
+        if (contains) {
+          writer.formattedLine("- {0, item}", NamedTextColor.AQUA, itemStack);
+        } else {
+          writer.formattedLine("- {0, item}", NamedTextColor.GRAY, itemStack);
+        }
+      }
+
+      return true;
+    });
+
+    if (!written) {
+      return null;
+    }
+
+    return writer.asComponent();
   }
 }
