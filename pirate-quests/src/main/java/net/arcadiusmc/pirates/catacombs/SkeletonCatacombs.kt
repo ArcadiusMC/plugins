ec3f8d867cf8013e1dc757ec8fed6ff6f9f4df24
andero-magi <andero.magi.420+schoolgit@gmail.com>
2024-09-26 11:48:12 +0300

    Delay skeleton despawn when leaving the catacomb area

--- pirate-quests/src/main/java/net/arcadiusmc/pirates/catacombs/SkeletonCatacombs.kt
+++ pirate-quests/src/main/java/net/arcadiusmc/pirates/catacombs/SkeletonCatacombs.kt
@@ -4,18 +4,29 @@ import net.arcadiusmc.pirates.getRandomSkeletonType
 import net.arcadiusmc.pirates.riseFromTheGrave
 import net.arcadiusmc.pirates.spawnSkeletonTypeAt
 import net.arcadiusmc.utils.Tasks
+import net.arcadiusmc.utils.math.Vectors
 import net.arcadiusmc.utils.math.WorldBounds3i
 import org.bukkit.Location
 import org.bukkit.World
+import org.bukkit.scheduler.BukkitTask
 import org.joml.Vector3d
 import java.util.*
 
-class CatacombState {
+enum class CatacombState {
+  INACTIVE,
+  SPAWNED,
+  AWAITING_DESPAWN
+}
+
+class Catacombs {
   val random: Random = Random()
-  var spawned: Boolean = false
+  var state: CatacombState = CatacombState.INACTIVE
+
+  var killTask: BukkitTask? = null
 }
 
 const val SKELETON_TAG = "blunderbeard.catacomb_guard"
+const val DESPAWN_DELAY = 30L * 20L
 
 private val spawns: List<Vector3d> = listOf(
   Vector3d(5554.5, 63.0, 1736.5),
@@ -31,33 +42,43 @@ private val spawns: List<Vector3d> = listOf(
   Vector3d(5545.5, 64.0, 1720.5)
 )
 
-private val currentState = CatacombState()
+private val currentState = Catacombs()
 
+// Called with a script from the usable trigger that
+// encapsulates the catacomb entry area
 fun spawn(world: World) {
-  if (currentState.spawned) {
+  if (currentState.state == CatacombState.SPAWNED) {
     return
   }
 
-  for (spawn in spawns) {
-    val pitch = currentState.random.nextFloat(-22.5f, 22.5f)
-    val yaw = currentState.random.nextFloat(-180f, 180f)
+  if (currentState.state == CatacombState.INACTIVE) {
+    for (spawn in spawns) {
+      val pitch = currentState.random.nextFloat(-22.5f, 22.5f)
+      val yaw = currentState.random.nextFloat(-180f, 180f)
 
-    val location = Location(world, spawn.x, spawn.y - 2.0, spawn.z, yaw, pitch)
-    val type = getRandomSkeletonType(currentState.random)
+      val location = Location(world, spawn.x, spawn.y - 2.0, spawn.z, yaw, pitch)
+      val type = getRandomSkeletonType(currentState.random)
 
-    val skeleton = spawnSkeletonTypeAt(type, location, currentState.random)
+      val skeleton = spawnSkeletonTypeAt(type, location, currentState.random)
 
-    skeleton.removeWhenFarAway = false
-    skeleton.isPersistent = true
-    skeleton.setShouldBurnInDay(false)
-    skeleton.addScoreboardTag(SKELETON_TAG)
+      skeleton.removeWhenFarAway = false
+      skeleton.isPersistent = true
+      skeleton.setShouldBurnInDay(false)
+      skeleton.addScoreboardTag(SKELETON_TAG)
 
-    riseFromTheGrave(skeleton)
+      riseFromTheGrave(skeleton)
+    }
   }
 
-  currentState.spawned = true
+  currentState.state = CatacombState.SPAWNED
+
+  if (Tasks.isScheduled(currentState.killTask)) {
+    Tasks.cancel(currentState.killTask)
+    currentState.killTask = null
+  }
 }
 
+// Called in the same way as the above method
 fun onLeaveArea(boundingBox: WorldBounds3i) {
   Tasks.runLater({
     val players = boundingBox.players
@@ -65,14 +86,49 @@ fun onLeaveArea(boundingBox: WorldBounds3i) {
       return@runLater
     }
 
-    currentState.spawned = false
+    val task = Tasks.runLater(KillTask(boundingBox), DESPAWN_DELAY)
 
-    val entities = boundingBox.getEntities {
-      return@getEntities it.scoreboardTags.contains(SKELETON_TAG)
-    }
-
-    for (entity in entities) {
-      entity.remove()
-    }
+    currentState.killTask = task
+    currentState.state = CatacombState.AWAITING_DESPAWN
   }, 1)
+}
+
+private class KillTask(val boundingBox: WorldBounds3i): Runnable {
+
+  override fun run() {
+    killEntities()
+
+    currentState.state = CatacombState.INACTIVE
+    currentState.killTask = null
+  }
+
+  fun killEntities() {
+    val a = 1 shr 2
+
+    val chunkBounds = boundingBox.set(
+      Vectors.toChunk(boundingBox.minX()),
+      Vectors.toChunk(boundingBox.minY()),
+      Vectors.toChunk(boundingBox.minZ()),
+      Vectors.toChunk(boundingBox.maxX()),
+      Vectors.toChunk(boundingBox.maxY()),
+      Vectors.toChunk(boundingBox.maxZ())
+    )
+
+    val world = boundingBox.world
+
+    for (x in chunkBounds.minX() until chunkBounds.maxX()) {
+      for (z in chunkBounds.minZ() until chunkBounds.maxZ()) {
+        val chunk = world.getChunkAt(x, z)
+        val entities = chunk.entities
+
+        for (entity in entities) {
+          if (!entity.scoreboardTags.contains(SKELETON_TAG)) {
+            continue
+          }
+
+          entity.remove()
+        }
+      }
+    }
+  }
 }
