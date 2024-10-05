b915db98809f13adb383d80f9acfc7de1ebf8ec7
andero-magi <andero.magi.420@gmail.com>
2024-10-05 10:47:47 +0300

    Javadoc

--- commons/src/main/java/net/arcadiusmc/ArcadiusServer.java
+++ commons/src/main/java/net/arcadiusmc/ArcadiusServer.java
@@ -1,23 +1,42 @@
 package net.arcadiusmc;
 
+import net.arcadiusmc.command.settings.SettingsBook;
 import net.arcadiusmc.text.Text;
 import net.arcadiusmc.text.ViewerAwareMessage;
 import net.arcadiusmc.text.channel.MessageRenderer;
 import net.arcadiusmc.user.User;
-import net.arcadiusmc.command.settings.SettingsBook;
 import net.kyori.adventure.text.ComponentLike;
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Service for general functions
+ */
 public interface ArcadiusServer {
 
+  /**
+   * Get the server instance
+   * @return Server instance
+   */
   static ArcadiusServer server() {
     return ServiceInstances.getServer();
   }
 
+  /**
+   * Get the current server spawn.
+   * <p>
+   * If the server spawn is not explicitly set, then this will return the spawn
+   * location of the overworld.
+   *
+   * @return Server spawn
+   */
   @NotNull
   Location getServerSpawn();
 
+  /**
+   * Set the current server spawn.
+   * @param serverSpawn New server spawn.
+   */
   void setServerSpawn(@NotNull Location serverSpawn);
 
   @NotNull
@@ -31,11 +50,62 @@ public interface ArcadiusServer {
     announce(viewer -> Text.valueOf(like, viewer));
   }
 
+  /**
+   * Register a listener for the {@code /leave} command.
+   *
+   * @param id Listener ID
+   * @param listener Listener callback
+   *
+   * @throws NullPointerException If either {@code id} or {@code listener} is {@code null}
+   */
   void registerLeaveListener(String id, LeaveCommandListener listener);
 
+  /**
+   * Unregister a listener for the {@code /leave} command.
+   *
+   * @param id Listener ID
+   *
+   * @throws NullPointerException If {@code id} is {@code null}
+   */
   void unregisterLeaveListener(String id);
 
+  /**
+   * Spawn a pile of coins that can be picked up by players.
+   *
+   * @param value The amount of money the pile will reward.
+   * @param pileSize The model size to use.
+   */
+  void spawnCoinPile(int value, CoinPileSize pileSize);
+
+  /**
+   * Coin pile size
+   */
+  enum CoinPileSize {
+    SMALL,
+    MEDIUM,
+    LARGE,
+    ;
+  }
+
+  /**
+   * {@code /leave} command callback.
+   */
   interface LeaveCommandListener {
+
+    /**
+     * Called when a player runs the {@code /leave} command.
+     * <p>
+     * If this method returns {@code true}, the system stops iterating through
+     * listeners and stops successfully.
+     * <p>
+     * If this method returns {@code false}, the system will move on to the next
+     * listener and invoke it until a successful result is found, or until all
+     * listeners have been executed.
+     *
+     * @param player Player that executed the command.
+     * @return {@code true}, if the player was able to use the command here,
+     *         {@code false} otherwise.
+     */
     boolean onUse(User player);
   }
 }
