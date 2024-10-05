0487b7329003a53a1406bdbccc0ae6c8c18a5f00
andero-magi <andero.magi.420@gmail.com>
2024-10-05 10:26:51 +0300

    Delete old dungeons module

--- dungeons/src/main/java/net/arcadiusmc/dungeons/LevelFunctions.java
+++
@@ -1,8 +0,0 @@
-package net.arcadiusmc.dungeons;
-
-public final class LevelFunctions {
-  private LevelFunctions() {}
-
-  public static final String CONNECTOR = "dungeons/connector";
-  public static final String TREASURE = "dungeons/treasure";
-}
