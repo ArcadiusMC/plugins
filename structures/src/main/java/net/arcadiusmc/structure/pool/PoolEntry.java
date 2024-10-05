cbc9eb7e37f3c130a9280cb0a1dfb8548fb8c4a0
andero-magi <andero.magi.420@gmail.com>
2024-10-05 10:33:38 +0300

    Better structure pool entry loading

--- structures/src/main/java/net/arcadiusmc/structure/pool/PoolEntry.java
+++ structures/src/main/java/net/arcadiusmc/structure/pool/PoolEntry.java
@@ -1,17 +1,21 @@
 package net.arcadiusmc.structure.pool;
 
+import static net.arcadiusmc.structure.BlockStructure.DEFAULT_PALETTE_NAME;
+
 import com.google.common.base.Strings;
 import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.arcadiusmc.registry.Registries;
-import net.arcadiusmc.structure.BlockStructure;
 import net.arcadiusmc.utils.io.Results;
 
 public record PoolEntry(int weight, String structureName, String paletteName) {
+
+  public static final int DEFAULT_WEIGHT = 1;
 
   public static Pattern SPLIT_PATTERN = Pattern.compile(
       "(" + Registries.VALID_KEY_REGEX + ")"
@@ -28,7 +32,7 @@ public record PoolEntry(int weight, String structureName, String paletteName) {
         String struct = matcher.group(1);
         String palette = Strings.nullToEmpty(matcher.group(2));
 
-        return Results.success(new PoolEntry(1, struct, palette));
+        return Results.success(new PoolEntry(DEFAULT_WEIGHT, struct, palette));
       },
       poolEntry -> {
         if (Strings.isNullOrEmpty(poolEntry.paletteName)) {
@@ -42,12 +46,26 @@ public record PoolEntry(int weight, String structureName, String paletteName) {
   private static final Codec<PoolEntry> RECORD_CODEC = RecordCodecBuilder.create(instance -> {
     return instance
         .group(
-            Codec.INT.optionalFieldOf("weight", 1).forGetter(o -> o.weight),
-            Codec.STRING.fieldOf("structure").forGetter(o -> o.structureName),
-            Codec.STRING.optionalFieldOf("palette", BlockStructure.DEFAULT_PALETTE_NAME)
-                .forGetter(o -> o.paletteName)
+            Codec.INT.optionalFieldOf("weight", DEFAULT_WEIGHT)
+                .forGetter(o -> o.weight),
+
+            Codec.STRING.fieldOf("structure")
+                .forGetter(o -> o.structureName),
+
+            Codec.STRING.optionalFieldOf("palette")
+                .forGetter(o -> {
+                  String str = o.paletteName;
+
+                  if (Strings.isNullOrEmpty(str) || str.equals(DEFAULT_PALETTE_NAME)) {
+                    return Optional.empty();
+                  }
+
+                  return Optional.of(str);
+                })
         )
-        .apply(instance, PoolEntry::new);
+        .apply(instance, (weight, struct, palette) -> {
+          return new PoolEntry(weight, struct, palette.orElse(""));
+        });
   });
 
   public static final Codec<PoolEntry> CODEC = Codec.either(STRING_CODEC, RECORD_CODEC)
@@ -62,6 +80,6 @@ public record PoolEntry(int weight, String structureName, String paletteName) {
       );
 
   public boolean isSimple() {
-    return weight == 1;
+    return weight == DEFAULT_WEIGHT;
   }
 }
