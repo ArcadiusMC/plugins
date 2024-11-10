ff675089aabefef12fb7a58bcfeb1783abdaeb59
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2024-11-10 16:13:53 +0200

    Allow setting skull builder profile with just texture link or id

--- commons/src/main/java/net/arcadiusmc/utils/inventory/SkullItemBuilder.java
+++ commons/src/main/java/net/arcadiusmc/utils/inventory/SkullItemBuilder.java
@@ -1,16 +1,23 @@
 package net.arcadiusmc.utils.inventory;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerTextures;
 
 @Getter
 public class SkullItemBuilder extends ItemBuilder<SkullItemBuilder> {
+
+  static final String BASE_URL = "http://textures.minecraft.net/texture/";
 
   public SkullItemBuilder(int amount) {
     super(Material.PLAYER_HEAD, amount);
@@ -24,7 +31,44 @@ public class SkullItemBuilder extends ItemBuilder<SkullItemBuilder> {
     return (SkullMeta) baseMeta;
   }
 
+  public SkullItemBuilder setTextureId(String textureId) {
+    if (textureId == null) {
+      return setProfile((PlayerProfile) null);
+    }
+
+    return setTextureUrl(BASE_URL + textureId);
+  }
+
+  public SkullItemBuilder setTextureUrl(String textureLink) {
+    if (textureLink == null) {
+      return setProfile((PlayerProfile) null);
+    }
+
+    SkullMeta meta = meta();
+    PlayerProfile profile = meta.getPlayerProfile();
+
+    if (profile == null) {
+      profile = Bukkit.createProfile(UUID.randomUUID());
+    }
+
+    PlayerTextures textures = profile.getTextures();
+
+    try {
+      textures.setSkin(new URL(textureLink));
+      profile.setTextures(textures);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+
+    return setProfile(profile);
+  }
+
   public SkullItemBuilder setProfile(PlayerProfile profile) {
+    if (profile == null) {
+      meta().setPlayerProfile(null);
+      return this;
+    }
+
     if (!profile.hasTextures()) {
       CompletableFuture.runAsync(() -> {
         profile.complete(true);
